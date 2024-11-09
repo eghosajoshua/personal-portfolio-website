ca558b701ff1b26f34fc181de5ad97bd8b6757eb
Eghosa Joshua <eghosajosh@gmail.com>
2024-11-09 17:34:53 +0100

    Update data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -22,16 +22,6 @@ export const data: {
   },
   {
     id: 2,
-    title: "2D Racing Game",
-    description: `2D CarRace Game is an engaging and entertaining car racing game developed using Pygame. Players navigate a car on a 2D track, aiming to achieve the best possible time while avoiding obstacles. The game features smooth graphics and responsive controls, providing an immersive racing experience. It also utilizes localStoragePy to save game data locally, ensuring that progress and high scores are preserved.`,
-    img: "/images/2dracegame.png",
-    type: "Desktop App",
-    github: "https://github.com/eghosajoshua/2d-topdown-cargame-using-pygame",
-    live: "",
-    technologies: ["Python", "Pygame"],
-  },
-  {
-    id: 3,
     title: "Personal Portfolio",
     description: `This portfolio website, built using React.js and Tailwind CSS, showcases a variety of projects and designs. The site leverages React.js to create a dynamic and interactive user experience, while Tailwind CSS is used to craft a sleek, responsive design with a utility-first approach. It features a gallery of projects.`,
     img: "/images/portfolio.png",
@@ -41,7 +31,7 @@ export const data: {
     technologies: ["ReactJs", "Typescript", "TailwindCSS"],
   },
   {
-    id: 4,
+    id: 3,
     title: "Ai Text to Voice",
     description: `Ai Text to Voice is an innovative web application designed to convert text into natural-sounding speech using the powerful 11 Labs API. Built with Vite and React.js, this application offers a seamless and efficient user experience. The intuitive interface allows users to easily input text and generate high-quality voice outputs, making it an ideal tool for various applications such as creating voiceovers, accessibility solutions, and more. `,
     img: "/images/aitexttovoice.png",
